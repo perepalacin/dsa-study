6ff6a112f5635d32beff687133366b46c25414c3
Pere Palacín <perepalacin@gmail.com>
2025-04-28 23:34:37 +0200

    saving because of power outage

--- src/main/java/a2z/dza/linked_lists/medium/OddEvenLinkedList.java
+++ src/main/java/a2z/dza/linked_lists/medium/OddEvenLinkedList.java
@@ -5,10 +5,23 @@ import a2z.dza.linked_lists.ListNode;
 
 public class OddEvenLinkedList {
 
-    public static ListNode oddEvenList (ListNode head) {
-        // First node is odd, second node is even 
-        // Put odds first, even after
+    public static ListNode oddEvenList(ListNode head) {
+        if  (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+
+        ListNode oddTail = head;
+        ListNode evenTail = head.next;
+        ListNode evenHead = head.next;
+
+        while (evenTail != null && evenTail.next != null) {
+            oddTail.next = evenTail.next;
+            oddTail = oddTail.next;
+            evenTail.next = oddTail.next;
+            evenTail = evenTail.next;
+        }
+
+        oddTail.next = evenHead;
         return head;
     }
-    
 }
